8e87ab32293f385030e98bbfc49833dda0832558
knguyen111601 <knguyen111601@gmail.com>
2022-03-31 11:05:16 -0700

    server running

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from flask import Flask
 
 # Create app 
 app = Flask(__name__)
+
+# Routing
+
+# Index route to "/"
+# @ is a decorator that extends a function or class
+@app.route("/")
+def index():
+    return "Hello World!"
+
+# Server Run like app.listen in Express
+app.run(host="localhost", port=3000)
