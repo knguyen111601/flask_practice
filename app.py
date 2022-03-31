83799d77b09a6e1e198b349f4bf4c633194f4318
knguyen111601 <knguyen111601@gmail.com>
2022-03-31 11:10:32 -0700

    created paramter and query route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 # Create app 
 app = Flask(__name__)
@@ -7,9 +7,16 @@ app = Flask(__name__)
 
 # Index route to "/"
 # @ is a decorator that extends a function or class
-@app.route("/")
+@app.get("/")
 def index():
     return "Hello World!"
+
+@app.get("/param/<my_param>")
+def param(my_param):
+    # access a url query
+    # /param/INSERTPARAMETER?query=somethingelse
+    query = request.args.get("query", None)
+    return {"param": my_param, "query": query}
 
 # Server Run like app.listen in Express
 app.run(host="localhost", port=3000)
